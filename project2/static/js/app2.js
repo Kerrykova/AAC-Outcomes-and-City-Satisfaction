ef88aca1e450cab40e377f2bff3e4f2f3874ce03
TitoOdunsi <43449274+TitoOdunsi@users.noreply.github.com>
2019-03-19 19:27:56 -0500

    changes

--- project2/static/js/app2.js
+++ project2/static/js/app2.js
@@ -1,72 +1,145 @@
-function buildCharts(sample) {
-  var sampleData;
-  var sample = 940
+function buildPieCharts(animal) {
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.json(`/samples/${sample}`).then(function(response){
+d3.json(`/aacdata`).then(function(response){
     // @TODO: Build a Bubble Chart using the sample data
-    sampleData = response;
-    // @TODO: Build a Pie Chart
-    //var outcomeAge = sampleData.outcomeAge;
-    //var type = sampleData.type;
-    //var breed = sampleData.breed;
+  animalData = response;
+
+  var aacDog = [];
+  var aacCat = [];
+
+  animalData.forEach(element => {
+  if (element.animal_type === "Dog"){
+    aacDog.push(element)
+  }
+  if (element.animal_type === "Cat") {
+    aacCat.push(element)
+  }
+
+  });
+  });
+}
+  
+  function wordCount(data) {
+    //   // An object to hold word frequency
+      var wordFrequency = {};
+    
+    //   // Iterate through the array
+      for (var i = 0; i < data.length; i++) {
+        var currentWord = data[i];
+        // If the word has been seen before...
+        if (currentWord in wordFrequency) {
+    //       // Add one to the counter
+          wordFrequency[currentWord] += 1;
+        }
+        else {
+          // Set the counter at 1
+          wordFrequency[currentWord] = 1;
+        }
+      }
+      var dogCount = wordFrequency.Dog;
+      var catCount = wordFrequency.Cat;
+      var otherCount = wordFrequency.Other;
+      var birdCount = wordFrequency.Bird;
+      var livestockCount = wordFrequency.Livestock;
+      var total = dogCount + catCount + otherCount + birdCount + livestockCount;
+      
+};
+  // function buildCharts(sample) {
+  // var sampleData;
+  // var sample = 940
+  // // @TODO: Use `d3.json` to fetch the sample data for the plots
+  // d3.json(`/samples/${sample}`).then(function(response){
+  //   // @TODO: Build a Bubble Chart using the sample data
+  //   sampleData = response;
+  //   // @TODO: Build a Pie Chart
+  //   //var outcomeAge = sampleData.outcomeAge;
+  //   //var type = sampleData.type;
+  //   //var breed = sampleData.breed;
     
     
-    //PIE CHART
+  //   //PIE CHART
 
-    var outcomeAge = outcomeAge;
-    var type = type;
-    var breed = breed.slice(0,10);
+  //   var outcomeAge = outcomeAge;
+  //   var type = type;
+  //   var breed = breed.slice(0,10);
 
-    var outcomeAgeLabels = outcomeAgeLabels;
-    var typeLabels = typeLabels;
-    var breedLabels = breedLabels;
+  //   var outcomeAgeLabels = outcomeAgeLabels;
+  //   var typeLabels = typeLabels;
+  //   var breedLabels = breedLabels;
 
-    var TypeData = [{
-      values: type,
-      labels: typeLabels,
-      type: 'pie',
-      // hovertext: topLabels,
-      // hoverinfo: 'label+text+value+percent',
-    }];
+  //   var TypeData = [{
+  //     values: type,
+  //     labels: typeLabels,
+  //     type: 'pie',
+  //     // hovertext: topLabels,
+  //     // hoverinfo: 'label+text+value+percent',
+  //   }];
     
-    var BreedData = [{
-      values: breed,
-      labels: breedLabels,
-      type: 'pie',
-      // hovertext: topLabels,
-      // hoverinfo: 'label+text+value+percent',
-    }];
+  //   var BreedData = [{
+  //     values: breed,
+  //     labels: breedLabels,
+  //     type: 'pie',
+  //     // hovertext: topLabels,
+  //     // hoverinfo: 'label+text+value+percent',
+  //   }];
 
-    var outcomeAgeData = [{
-      values: outcomeAge,
-      labels: outcomeAgeLabels,
-      type: 'pie',
-      // hovertext: topLabels,
-      // hoverinfo: 'label+text+value+percent',
-    }];
+  //   var outcomeAgeData = [{
+  //     values: outcomeAge,
+  //     labels: outcomeAgeLabels,
+  //     type: 'pie',
+  //     // hovertext: topLabels,
+  //     // hoverinfo: 'label+text+value+percent',
+  //   }];
 
 
-    var pieLayout = {
-      height: 500,
-      width: 500
-    };
+  //   var pieLayout = {
+  //     height: 500,
+  //     width: 500
+  //   };
     
-    Plotly.newPlot('pie', pieData, pieLayout);
+  //   Plotly.newPlot('pie', pieData, pieLayout);
 
   
-  //create the chart
-    var chart = animalType.pie();
-    var chart = animalBreed.pie();
-    var chart = animaloutcomeAge.pie();
+  // //create the chart
+  //   var chart = animalType.pie();
+  //   var chart = animalBreed.pie();
+  //   var chart = animaloutcomeAge.pie();
 
-  //set the chart title
-  AustinAnimals.title;
+  // //set the chart title
+  // AustinAnimals.title;
 
-  //add the data
-  chart.data(data);
+  // //add the data
+  // chart.data(data);
 
-  //display the chart in the container
-  chart.container('container');
-  chart.draw();
-  }
-  )};
+  // //display the chart in the container
+  // chart.container('container');
+  // chart.draw();
+  // }
+  // )};
+
+  function drawChart() {
+    var myConfig = {
+      "type":"pie",
+      "title":{
+        "text":"Pie Chart"
+      },
+      "series":[
+        {"values":[dogCount]},
+        {"values":[catCount]},
+        {"values":[otherCount]},
+        {"values":[birdCount]},
+        {"values":[livestockCount]}
+      ]
+    };
+     
+    zingchart.render({ 
+      id : 'pie', 
+      data : myConfig, 
+      height: 400, 
+      width: "100%" 
+    });
+  };
+  function init() {
+    buildPieCharts();
+    drawchart();
+  };
